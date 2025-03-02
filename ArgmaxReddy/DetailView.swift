98fa27804c8d402944b9026267b486baab77494c
vreddynyc <bonnievijay@mac.mynetworksettings.com>
2025-03-02 00:59:23 -0500

    show model in detail view

--- ArgmaxReddy/DetailView.swift
+++ ArgmaxReddy/DetailView.swift
@@ -1,17 +1,37 @@
 import SwiftUI
+import Kingfisher
 
 struct DetailView: View {
     
-    private var selectedIndex: Int
+    @EnvironmentObject var viewModel: ViewModel
     
-    init(selectedIndex: Int) {
-        self.selectedIndex = selectedIndex
+    private let index: Int
+    
+    init(index: Int) {
+        self.index = index
     }
-    
+            
     var body: some View {
-        let defaults = UserDefaults.standard
-        if let modelDataText = defaults.string(forKey: "key" + String(selectedIndex)) {
-            Text(modelDataText)
-        }
+        
+        let item = viewModel.userList[index]
+        
+        Text(item.display_name)
+            .font(.headline)
+        
+        KFImage(URL(string: item.profile_image))
+            .onSuccess { result in
+                print("Image loaded successfully: \(result.cacheType)")
+            }
+            .onFailure { error in
+                print("Image failed to load: \(error.localizedDescription)")
+            }
+            .onProgress { receivedSize, totalSize in
+                print("Loading progress: \(receivedSize)/\(totalSize)")
+            }
+            .resizable()
+            .scaledToFit()
+            .frame(width: 220, height: 220)
+        
+        Text((viewModel.modelResultMap[index] ?? [])[1])
     }
 }
